8cd8d921733645a5e2a8644df5f71d07f471b60f
Rodrigo Martins <rodrigomj@icloud.com>
2022-07-04 17:14:41 -0300

    Adicionar arquivos de projeto.

---
+++ Enum/TipoConta.cs
@@ -0,0 +1,8 @@
+﻿namespace bancoDio.Enum
+{
+    enum TipoConta
+    {
+        PessoaFisica = 1,
+        PessoaJuridica = 2
+    }
+}
---
+++ Model/Conta.cs
@@ -0,0 +1,58 @@
+﻿using bancoDio.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace bancoDio.Model
+{
+    class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tpConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tpConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito *-1))
+            {
+                Console.WriteLine("Não foi possivel realzar esta operação. Entre em contato");
+                return false;
+            }
+            this.Saldo -= valorSaque;
+            Console.WriteLine("{1}, Saque realizado com sucesso. Seu saldo é R$ {0} ", this.Saldo, this.Nome);
+            return true;
+        }
+
+        public void Deposito(double val) 
+        {
+            this.Saldo += val;
+            Console.WriteLine("{0}, seu saldo atual é R$ {1}", this.Nome, this.Saldo);
+        }
+
+        public  void Transferir(double val, Conta destino) 
+        {
+            if (this.Sacar(val)) destino.Deposito(val);
+           
+        }
+
+        public override string ToString() 
+        {
+            string data = "";
+            data += "Tipo de Conta: " + this.TipoConta + " | ";
+            data += "Titular : " + this.Nome + " | ";
+            data += "Saldo: " + this.Saldo + " | ";
+            data += "Crédito: " + this.Credito;
+            return data;
+        }
+    }
+}
